890bef63d679c3fd3986c6b83c179787e170707f
Jakub Robaczewski <kubarob13@gmail.com>
2023-04-29 17:26:54 +0200

    Added new skills tab to wfrp

--- module/systemProviders/wfrp4eProvider.js
+++ module/systemProviders/wfrp4eProvider.js
@@ -7,7 +7,8 @@ export class wfrp4eProvider extends SystemProvider {
 
   get tabs() {
     return {
-      currencies: { id: "currencies", visible: true, localization: "party-overview.WEALTH" },
+      currencies: { id: "currencies", visible: true, localization: "Money" },
+      lore: { id: "skills", visible: true, localization: "Skills" },
     };
   }
 
@@ -15,7 +16,8 @@ export class wfrp4eProvider extends SystemProvider {
     return "/modules/party-overview/templates/wfrp4e.hbs";
   }
 
-  getCurrency(money) {
+  getCurrency(actor) {
+    const money = actor.getItemTypes("money").map(m => m.toObject());
     const currency = {
       bp: money.find(i => i.name === game.i18n.localize("NAME.BP")).system.quantity.value,
       ss: money.find(i => i.name === game.i18n.localize("NAME.SS")).system.quantity.value,
@@ -25,10 +27,42 @@ export class wfrp4eProvider extends SystemProvider {
     currency.total = (currency.bp / 240 + currency.ss / 20 + currency.gc).toFixed(2)
     return currency
   }
+  getSkills(actor) {
+    let skills = actor.getItemTypes("skill")
+      .filter(skill => skill.system.advances.value > 0)
+      .sort(skill => skill.name)
+      .map(skill => ({
+        name: skill.name,
+        nameSpec: skill.name.substring(skill.name.indexOf("(")+1, skill.name.length-1),
+        total: skill.system.total.value,
+        advanced: skill.system.advanced.value
+      }))
+
+    let meleeRanged = skills.filter(skill => skill.name.includes(game.i18n.localize("NAME.Melee")) || skill.name.includes(game.i18n.localize("NAME.Ranged")))
+    let languages = skills.filter(skill => skill.name.includes(game.i18n.localize("NAME.Language")))
+    let lore = skills.filter(skill => skill.name.includes(game.i18n.localize("NAME.Lore")))
+    let trade = skills.filter(skill => skill.name.includes(game.i18n.localize("NAME.Trade")))
+
+    let otherBasic = skills.filter(skill => skill.advanced === "bsc" && !meleeRanged.includes(skill))
+    let otherAdvanced = skills.filter(skill =>
+      !meleeRanged.includes(skill) &&
+      !languages.includes(skill) &&
+      !lore.includes(skill) &&
+      !trade.includes(skill) &&
+      !otherBasic.includes(skill))
+
+    return {
+      meleeRanged: meleeRanged,
+      languages: languages,
+      lore: lore,
+      trade: trade,
+      otherBasic: otherBasic,
+      otherAdvanced: otherAdvanced
+    }
+  }
 
   getActorDetails(actor) {
     const data = actor.system;
-    const money = actor.getItemTypes("money").map(m => m.toObject());
     return {
       id: actor.id,
       name: actor.name,
@@ -42,7 +76,13 @@ export class wfrp4eProvider extends SystemProvider {
       fate: data.status.fate.value,
       resilience: data.status.resilience.value,
       resolve: data.status.resolve.value,
-      currency: this.getCurrency(money)
+      corruption: {
+        value: data.status.corruption.value,
+        max: data.status.corruption.max,
+      },
+      status: data.details.status.value,
+      skills: this.getSkills(actor),
+      currency: this.getCurrency(actor)
     };
   }
 
@@ -59,8 +99,6 @@ export class wfrp4eProvider extends SystemProvider {
       totalCurrency.gc += actor.currency.gc;
     }
     totalCurrency.total = (totalCurrency.bp / 240 + totalCurrency.ss / 20 + totalCurrency.gc).toFixed(2)
-    console.log(actors)
-    console.log(totalCurrency)
     return [
       actors,
       {
