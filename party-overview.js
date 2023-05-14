b9cf3052420978a5585d2147177cb709519fbc58
Matheus Clemente <matheus.xb@gmail.com>
2023-05-14 17:46:54 -0300

    Move Party Overview into game scope

--- party-overview.js
+++ party-overview.js
@@ -1,21 +1,19 @@
 import { currentSystemProvider, initApi } from "./module/api.js";
 import PartyOverviewApp from "./module/logic.js";
-import { registerSettings, registerApiSettings } from "./module/settings.js";
-
-let partyOverview;
+import { registerApiSettings, registerSettings } from "./module/settings.js";
 
 Hooks.once("init", () => {
 	registerSettings();
 	initApi();
 	registerApiSettings();
-	partyOverview = new PartyOverviewApp();
+	game.partyOverview = new PartyOverviewApp();
 
 	if (game.keybindings) {
 		game.keybindings.register("party-overview", "openPartyOverview", {
 			name: game.i18n.localize("party-overview.keybinds.open.name"),
 			hint: game.i18n.localize("party-overview.keybinds.open.hint"),
 			onDown: () => {
-				partyOverview.render(true);
+				game.partyOverview.render(true);
 			},
 			onUp: () => {},
 			precedence: CONST.KEYBINDING_PRECEDENCE.NORMAL,
@@ -24,7 +22,7 @@ Hooks.once("init", () => {
 			name: game.i18n.localize("party-overview.keybinds.close.name"),
 			hint: game.i18n.localize("party-overview.keybinds.close.hint"),
 			onDown: () => {
-				partyOverview.close();
+				game.partyOverview.close();
 			},
 			onUp: () => {},
 			precedence: CONST.KEYBINDING_PRECEDENCE.NORMAL,
@@ -33,8 +31,8 @@ Hooks.once("init", () => {
 			name: game.i18n.localize("party-overview.keybinds.toggle.name"),
 			hint: game.i18n.localize("party-overview.keybinds.toggle.hint"),
 			onDown: () => {
-				if (!partyOverview.rendering) partyOverview.render(true);
-				else partyOverview.close();
+				if (!game.partyOverview.rendering) game.partyOverview.render(true);
+				else game.partyOverview.close();
 			},
 			onUp: () => {},
 			precedence: CONST.KEYBINDING_PRECEDENCE.NORMAL,
@@ -50,57 +48,63 @@ Hooks.once("init", () => {
 });
 
 Hooks.on("ready", () => {
-	if (!partyOverview) partyOverview = new PartyOverviewApp();
-	if (game.modules.get("stream-view")?.active && game?.user?.id === game.settings.get("stream-view", "user-id")) partyOverview.render(true);
+	if (!game.partyOverview) game.partyOverview = new PartyOverviewApp();
+	if (game.modules.get("stream-view")?.active && game?.user?.id === game.settings.get("stream-view", "user-id"))
+		game.partyOverview.render(true);
 });
 
 Hooks.on("renderActorDirectory", (app, html, data) => {
 	if (!game.user.isGM && !game.settings.get("party-overview", "EnablePlayerAccess")) return;
 
-	let button = $(`<button class="party-overview-button ${currentSystemProvider.customCSS}"><i class="fas fa-users"></i> Party Overview</button>`);
+	let button = $(
+		`<button class="party-overview-button ${currentSystemProvider.customCSS}"><i class="fas fa-users"></i> Party Overview</button>`
+	);
 	button.on("click", (e) => {
-		partyOverview.render(true);
+		game.partyOverview.render(true);
 	});
 
 	$(html).find(".header-actions").prepend(button);
 });
 
 Hooks.on("updateActor", (actor, data, options, userId) => {
-	if (partyOverview.rendering && actor.hasPlayerOwner) {
-		partyOverview.render(false);
+	if (game.partyOverview.rendering && actor.hasPlayerOwner) {
+		game.partyOverview.render(false);
 	}
 });
 
 Hooks.on("updateToken", (token, data, options, userId) => {
-	if (partyOverview.rendering && token.actor?.hasPlayerOwner) {
-		partyOverview.render(false);
+	if (game.partyOverview.rendering && token.actor?.hasPlayerOwner) {
+		game.partyOverview.render(false);
 	}
 });
 
 Hooks.on("createToken", (token, options, userId) => {
-	if (partyOverview.rendering && game.actors.contents.find((actor) => actor.id === token.actor.id).hasPlayerOwner) {
-		partyOverview.render(false);
+	if (
+		game.partyOverview.rendering &&
+		game.actors.contents.find((actor) => actor.id === token.actor.id).hasPlayerOwner
+	) {
+		game.partyOverview.render(false);
 	}
 });
 
 Hooks.on("deleteActor", (actor, options, userId) => {
-	if (partyOverview.rendering && actor.hasPlayerOwner) {
-		partyOverview.render(false);
+	if (game.partyOverview.rendering && actor.hasPlayerOwner) {
+		game.partyOverview.render(false);
 	}
 });
 
 Hooks.on("deleteToken", (token, options, userId) => {
-	if (partyOverview.rendering && token.actor?.hasPlayerOwner) {
-		partyOverview.render(false);
+	if (game.partyOverview.rendering && token.actor?.hasPlayerOwner) {
+		game.partyOverview.render(false);
 	}
 });
 
 Hooks.on("canvasInit", (canvas) => {
-	if (partyOverview.rendering) {
+	if (game.partyOverview.rendering) {
 		// what a hack! the hook is fired when the scene switch is not yet activated, so we need
 		// to wait a tiny bit. The combat tracker is rendered last, so the scene should be available
 		Hooks.once("renderCombatTracker", (app, html, data) => {
-			partyOverview.render(false);
+			game.partyOverview.render(false);
 		});
 	}
 });
